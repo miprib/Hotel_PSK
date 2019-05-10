8df0856f27bfe144b720f65866d9d8578671e5ed
Mikas <mikas.prib@gmail.com>
2019-05-10 20:32:25 +0300

    Deltaspike

---
+++ src/main/java/usecases/SimulateWork.java
@@ -0,0 +1,40 @@
+package usecases;
+
+import services.BTCMiningService;
+
+import javax.enterprise.context.SessionScoped;
+import javax.enterprise.inject.Model;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import java.io.Serializable;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+@SessionScoped
+@Model
+public class SimulateWork implements Serializable {
+    @Inject
+    BTCMiningService BTCMiningService;
+
+    private Future<Integer> miningTask = null;
+
+    public String mineBTC() {
+        miningTask = BTCMiningService.mineBTC();
+
+        return  "mineBTC.xhtml?faces-redirect=true";
+    }
+
+    public boolean isMining() {
+        return miningTask != null && !miningTask.isDone();
+    }
+
+    public String getMiningStatus() throws ExecutionException, InterruptedException {
+        if (miningTask == null) {
+            return null;
+        } else if (isMining()) {
+            return "Mining in progress";
+        }
+        return "BTC mined: " + miningTask.get();
+    }
+}
