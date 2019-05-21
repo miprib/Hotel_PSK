061a6d2e08e8a2d4065c8e5de079c75fe0255134
Mikas <mikas.prib@gmail.com>
2019-05-21 18:55:47 +0300

    Hotel entity exposed as API

---
+++ src/main/java/api/controllers/ReservationController.java
@@ -0,0 +1,84 @@
+package api.controllers;
+
+import api.dto.HotelDTO;
+import dao.HotelDAO;
+import entities.Hotel;
+import interceptors.LoggedInvocation;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.OptimisticLockException;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@ApplicationScoped
+@Path("/hotel")
+public class ReservationController {
+
+    @Inject
+    @Setter
+    @Getter
+    private HotelDAO hotelDAO;
+
+    @Path("/{id}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @LoggedInvocation
+    public Response get(@PathParam("id") final Long id) {
+        Hotel hotel = hotelDAO.findHotel(id);
+        if (hotel == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        HotelDTO hotelDTO = new HotelDTO();
+
+        hotelDTO.setName(hotel.getName());
+        hotelDTO.setStreetAddress(hotel.getStreetAddress());
+
+        return Response.ok(hotelDTO).build();
+    }
+
+    @Path("/{id}")
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @LoggedInvocation
+    @Transactional
+    public Response put(
+            @PathParam("id") final Long id,
+            HotelDTO hotelDTO) {
+        try {
+            Hotel hotel = hotelDAO.findHotel(id);
+            if (hotel == null) {
+                return Response.status(Response.Status.NOT_FOUND).build();
+            }
+
+            hotel.setName(hotelDTO.getName());
+            hotel.setStreetAddress(hotelDTO.getStreetAddress());
+
+            hotelDAO.updateHotel(hotel);
+
+            return Response.ok().build();
+        } catch (OptimisticLockException ole) {
+            return Response.status(Response.Status.CONFLICT).build();
+        }
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @LoggedInvocation
+    @Transactional
+    public Response post(HotelDTO hotelDTO) {
+        Hotel hotel = new Hotel();
+
+        hotel.setName(hotelDTO.getName());
+        hotel.setStreetAddress(hotelDTO.getStreetAddress());
+
+        hotelDAO.addHotel(hotel);
+
+        return Response.ok().build();
+    }
+}
---
+++ src/main/java/api/dto/HotelDTO.java
@@ -0,0 +1,10 @@
+package api.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class HotelDTO {
+    private String name;
+    private String streetAddress;
+}
